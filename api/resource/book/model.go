f8b43c5e3014d9281e04d4dd7781f3941cdf1b0e
Adwaith Rajeev <adwaithrajeev@gmail.com>
2024-12-15 20:45:19 +0530

    Added data models for book, DTO and form.

---
+++ api/resource/book/model.go
@@ -0,0 +1,38 @@
+package book
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Book struct {
+	ID            uuid.UUID `gorm:"primarykey"`
+	Title         string
+	Author        string
+	PublishedDate time.Time
+	ImageURL      string
+	Description   string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt
+}
+
+type Books []*Book
+
+type DTO struct {
+	ID            string `json:"id"`
+	Title         string `json:"title"`
+	Author        string `json:"author"`
+	PublishedDate string `json:"published_date"`
+	ImageURL      string `json:"image_url"`
+	Description   string `json:"description"`
+}
+
+type Form struct {
+	Title         string `json:"title"`
+	Author        string `json:"author"`
+	PublishedDate string `json:"published_date"`
+	ImageURL      string `json:"image_url"`
+	Description   string `json:"description"`
+}
