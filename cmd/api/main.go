9e83975234a71cbef30087f4d21a71674efa9731
Adwaith Rajeev <adwaithrajeev@gmail.com>
2024-12-15 16:01:12 +0530

    Added changes to load config.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	"book-shelf/config"
 	"errors"
 	"io"
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
+	c := config.NewConfig()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", hello)
 
 	s := &http.Server{
 		Addr:         ":8080",
 		Handler:      mux,
-		ReadTimeout:  2 * time.Second,
-		WriteTimeout: 2 * time.Second,
-		IdleTimeout:  5 * time.Second,
+		ReadTimeout:  c.Server.ReadTimeout,
+		WriteTimeout: c.Server.WriteTimeout,
+		IdleTimeout:  c.Server.IdleTimeout,
 	}
 
-	log.Println("Starting server :8080")
+	log.Println("Starting server " + s.Addr)
 	if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal("Server startup failed")
 	}
