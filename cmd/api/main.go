d14025ec08fff00a9f0741bb3931bf90fe923133
Adwaith Rajeev <adwaithrajeev@gmail.com>
2024-12-15 20:44:57 +0530

    Added router and OpenAPI specifications.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"book-shelf/api/router"
 	"book-shelf/config"
 	"errors"
-	"io"
 	"log"
 	"net/http"
 )
 
+//  @title          MYAPP API
+//  @version        1.0
+//  @description    This is a sample RESTful API with a CRUD
+
+//  @contact.name   Adwaith Rajeev
+//  @contact.url    adwaithrajeev@gmail.com
+
+//  @license.name   MIT License
+//  @license.url
+
+// @host       localhost:8080
+// @basePath   /v1
 func main() {
 	c := config.NewConfig()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", hello)
-
+	r := router.New()
 	s := &http.Server{
 		Addr:         ":8080",
-		Handler:      mux,
+		Handler:      r,
 		ReadTimeout:  c.Server.ReadTimeout,
 		WriteTimeout: c.Server.WriteTimeout,
 		IdleTimeout:  c.Server.IdleTimeout,
@@ -25,12 +35,5 @@ func main() {
 	log.Println("Starting server " + s.Addr)
 	if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal("Server startup failed")
-	}
-}
-
-func hello(w http.ResponseWriter, _ *http.Request) {
-	_, err := io.WriteString(w, "Hello, World!")
-	if err != nil {
-		return
 	}
 }
