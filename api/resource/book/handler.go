bc433873b9a954932cde3b099d46d0fcd117faef
Adwaith Rajeev <adwaithrajeev@gmail.com>
2024-12-19 23:53:08 +0530

    Added implementation for each handler.

--- api/resource/book/handler.go
+++ api/resource/book/handler.go
@@ -1,8 +1,23 @@
 package book
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"net/http"
+)
 
 type API struct {
+	repository *Repository
+}
+
+func New(db *gorm.DB) *API {
+	return &API{
+		repository: NewRepository(db),
+	}
 }
 
 // List godoc
@@ -15,7 +30,26 @@ type API struct {
 //	@success        200 {array}     DTO
 //	@failure        500 {object}    err.Error
 //	@router         /books [get]
-func (a *API) List(w http.ResponseWriter, r *http.Request) {
+func (a *API) List(w http.ResponseWriter, _ *http.Request) {
+	books, err := a.repository.List()
+	if err != nil {
+		// handle later
+		return
+	}
+
+	if len(books) == 0 {
+		_, err = fmt.Fprint(w, "[]")
+		if err != nil {
+			// handle later
+			return
+		}
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(books.ToDto()); err != nil {
+		// handle later
+		return
+	}
 }
 
 // Create godoc
@@ -32,6 +66,22 @@ func (a *API) List(w http.ResponseWriter, r *http.Request) {
 //	@failure        500 {object}    err.Error
 //	@router         /books [post]
 func (a *API) Create(w http.ResponseWriter, r *http.Request) {
+	form := &Form{}
+	if err := json.NewDecoder(r.Body).Decode(form); err != nil {
+		// handle later
+		return
+	}
+
+	newBook := form.ToModel()
+	newBook.ID = uuid.New()
+
+	_, err := a.repository.Create(newBook)
+	if err != nil {
+		// handle later
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
 
 // Read godoc
@@ -48,6 +98,28 @@ func (a *API) Create(w http.ResponseWriter, r *http.Request) {
 //	@failure        500 {object}    err.Error
 //	@router         /books/{id} [get]
 func (a *API) Read(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		// handle later
+		return
+	}
+
+	book, err := a.repository.Read(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		// handle later
+		return
+	}
+
+	dto := book.ToDto()
+	if err := json.NewEncoder(w).Encode(dto); err != nil {
+		// handle later
+		return
+	}
 }
 
 // Update godoc
@@ -66,6 +138,30 @@ func (a *API) Read(w http.ResponseWriter, r *http.Request) {
 //	@failure        500 {object}    err.Error
 //	@router         /books/{id} [put]
 func (a *API) Update(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		// handle later
+		return
+	}
+
+	form := &Form{}
+	if err := json.NewDecoder(r.Body).Decode(form); err != nil {
+		// handle later
+		return
+	}
+
+	book := form.ToModel()
+	book.ID = id
+
+	rows, err := a.repository.Update(book)
+	if err != nil {
+		// handle later
+		return
+	}
+	if rows == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 }
 
 // Delete godoc
@@ -82,4 +178,19 @@ func (a *API) Update(w http.ResponseWriter, r *http.Request) {
 //	@failure        500 {object}    err.Error
 //	@router         /books/{id} [delete]
 func (a *API) Delete(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		// handle later
+		return
+	}
+
+	rows, err := a.repository.Delete(id)
+	if err != nil {
+		// handle later
+		return
+	}
+	if rows == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 }
