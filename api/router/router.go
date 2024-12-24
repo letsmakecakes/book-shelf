f2c89d6cbbe3c67e59d9274ff353941f20e049a5
Adwaith Rajeev <adwaithrajeev@gmail.com>
2024-12-24 15:28:39 +0530

    Added validation.

--- api/router/router.go
+++ api/router/router.go
@@ -4,16 +4,17 @@ import (
 	"book-shelf/api/resource/book"
 	"book-shelf/api/resource/health"
 	"github.com/go-chi/chi"
+	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
-func New(db *gorm.DB) *chi.Mux {
+func New(db *gorm.DB, v *validator.Validate) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/health", health.Read)
 
 	r.Route("/v1", func(r chi.Router) {
-		bookAPI := book.New(db)
+		bookAPI := book.New(db, v)
 		r.Get("/books", bookAPI.List)
 		r.Post("/books", bookAPI.Create)
 		r.Get("/books/{id}", bookAPI.Read)
